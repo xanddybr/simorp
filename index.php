7dbbfa4c1fda33b7403a6d50a8b1305a28699ecf
xanddybr <xanddybr@gmail.com>
2024-05-23 00:05:15 -0300

    to be continue

--- index.php
+++ index.php
@@ -15,12 +15,12 @@
 
 <title>Bem Vindo ao SIMORP</title>
 
-<!-- load dependency libraries  -->
+<!-- load dependency libraries css  -->
 <link href="./css/bootstrap.min.css" rel="stylesheet">
 <link href="./css/styles.css" rel="stylesheet">
 <link href="./img/ico/pencil.ico" rel="icon" type="image/x-icon" >
 
-<!-- load dependency libraries -->
+<!-- load dependency libraries js -->
 <script src="./js/lumino.glyphs.js" type="text/javascript"></script>
 <script src="./js/jquery.min.js" type="text/javascript"></script>
 <script src="./js/index_set.js" type="text/javascript"></script>
@@ -29,6 +29,29 @@
 </head>
 
 <script>
+      
+
+<!-- Modal -->
+<div class="modal fade" tabindex="-1" role="dialog" id="exampleModalCenter" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p>Modal body text goes here.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" id="sl" class="btn btn-primary">Save changes</button>
+        <button type="button" id="cl" class="btn btn-secondary" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
+
 
 
 </script>
